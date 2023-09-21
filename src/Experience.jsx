e7658eedd93480fc2e61183016e83df92157f5ac
Fret <clemfret7@gmail.com>
2023-09-21 15:10:41 +0200

    marble-game

---
+++ src/Experience.jsx
@@ -0,0 +1,24 @@
+import {Physics} from "@react-three/rapier";
+
+import Lights from "./Lights.jsx";
+import { Level } from "./Level.jsx";
+import Player from "./Player.jsx";
+import useGame from "./stores/useGame.js";
+
+export default function Experience() {
+
+	const blocksCount = useGame((state) => state.blocksCount)
+	const blocksSeed = useGame((state) => state.blocksSeed)
+
+	return (
+		<>
+			<color args={[ '#bdedfc' ]} attach={'background'} />
+			<Physics>
+				<Lights />
+
+				<Level count={ blocksCount } seed={ blocksSeed } />
+				<Player />
+			</Physics>
+		</>
+	);
+}
---
+++ src/Interface.jsx
@@ -0,0 +1,61 @@
+import { useKeyboardControls } from "@react-three/drei";
+import useGame from "./stores/useGame";
+import { useEffect, useRef } from "react";
+import { addEffect } from "@react-three/fiber";
+
+export default function Interface() {
+
+  const time = useRef()
+
+  const forward = useKeyboardControls((state) => state.forward)
+  const backward = useKeyboardControls((state) => state.backward)
+  const leftward = useKeyboardControls((state) => state.leftward)
+  const rightward = useKeyboardControls((state) => state.rightward)
+  const jump = useKeyboardControls((state) => state.jump)
+
+  const phase = useGame((state) => state.phase)
+  const restart = useGame((state) => state.restart)
+
+  useEffect(() => {
+    const unsubscribeEffect = addEffect(() => {
+      const state = useGame.getState()
+
+      let elapsedTime = 0.00
+      if (state.phase === 'playing') {
+        elapsedTime = Date.now() - state.startTime
+      } else if (state.phase === 'ended') {
+        elapsedTime = state.endTime - state.startTime
+      }
+
+      elapsedTime /= 1000
+      elapsedTime = elapsedTime.toFixed(2)
+      if (time.current) {
+        time.current.textContent = elapsedTime
+      }
+    })
+
+    return () => {
+      unsubscribeEffect()
+    }
+  }, [])
+
+	return (
+		<div className="interface">
+			<div ref={ time } className="time">0.00</div>
+			{ phase === 'ended' ? <div className="restart" onClick={ restart }>Restart</div> : null}
+			<div className="controls">
+				<div className="raw">
+					<div className={`key ${ forward ? 'active' : '' }`}></div>
+				</div>
+				<div className="raw">
+					<div className={`key ${ leftward ? 'active' : '' }`}></div>
+					<div className={`key ${ backward ? 'active' : '' }`}></div>
+					<div className={`key ${ rightward ? 'active' : '' }`}></div>
+				</div>
+				<div className="raw">
+					<div className={`key large  ${ jump ? 'active' : '' }`}></div>
+				</div>
+			</div>
+		</div>
+	);
+}
